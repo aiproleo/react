5435a057a28f2562d6daf947cc329d97928eeefd
aiXpertLab <aiXpertLab@gmail.com>
2024-06-25 08:03:44 -0400

    v2

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,47 +5,54 @@ import { createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './error404.tsx'
 import Home from '../components/Home';
 import StateHooks from '../components/StateHooks/index.tsx';
-import UseState   from '../components/StateHooks/useState.tsx'
+import UseState from '../components/StateHooks/useState.tsx'
 import UseReducer from '../components/StateHooks/useReducer.tsx'
+import ReactContext from '../components/StateHooks/reactContext.tsx'
+import CountProvider from '../components/StateHooks/reactContext_';
+import TanStackQuery from '../components/StateHooks/tanstackquery.tsx';
 
 export const router = createBrowserRouter([
     {
-        path: "/",        element: <Root />,        errorElement: <ErrorPage />,
+        path: "/", element: <Root />, errorElement: <ErrorPage />,
         children: [
-            {path: "/",         element: <Home />,},
-            {path: "/statehooks",          element: <StateHooks />,},
-            {path: "/statehooks/useState", element: <UseState />,},
-            {path: "/statehooks/useReducer", element: <UseReducer />,},
+            { path: "/", element: <Home />, },
+            { path: "/statehooks", element: <StateHooks />, },
+            { path: "/statehooks/useState", element: <UseState />, },
+            { path: "/statehooks/useReducer", element: <UseReducer />, },
+            { path: "/statehooks/reactContext", element: <ReactContext />, },
+            { path: "/tanstackquery", element: <TanStackQuery />, },
         ],
     },
 ]);
 
 export default function Root() {
     return (
-        <div id="root">
-            <div id="sidebar">
-                <h1>OmniDevX React</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
-                        <div id="search-spinner" aria-hidden hidden={true} />
-                        <div className="sr-only" aria-live="polite"></div>
-                    </form>
-                    <form method="post">
-                        <button type="submit">New</button>
-                    </form>
+        <CountProvider>
+            <div id="root">
+                <div id="sidebar">
+                    <h1>OmniDevX React</h1>
+                    <div>
+                        <form id="search-form" role="search">
+                            <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
+                            <div id="search-spinner" aria-hidden hidden={true} />
+                            <div className="sr-only" aria-live="polite"></div>
+                        </form>
+                        <form method="post">
+                            <button type="submit">New</button>
+                        </form>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><Link to={`/`}>Home</Link></li>
+                            <li><Link to={"/statehooks"}>State Hooks</Link></li>
+                            <li><Link to={`/tanstackquery`}>TanStack Query</Link></li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul>
-                        <li><Link to={`/`}>Home</Link></li>
-                        <li><Link to={"/statehooks"}>State Hooks</Link></li>
-                        <li><Link to={`/contacts`}>Your Friend</Link></li>
-                    </ul>
-                </nav>
+                <div id="detail">
+                    <Outlet />
+                </div>
             </div>
-            <div id="detail">
-                <Outlet />
-            </div>
-        </div>
+        </CountProvider>
     );
 }
